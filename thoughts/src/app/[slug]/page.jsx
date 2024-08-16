98251d8a5b8e27cb95bd596848114b0245828cb2
Barry Richards <barry@br-web.me>
2024-08-16 10:43:42 +0100

    Single post page

--- thoughts/src/app/[slug]/page.jsx
+++ thoughts/src/app/[slug]/page.jsx
@@ -13,5 +13,30 @@ function getPostContent(slug){
 
 export const generateStaticParams = async () => {
 	const posts = getPostMetadata('posts');
+
 	return posts.map((post) => ({slug: post.slug}))
+}
+
+export async function generateMetadata({params, searchParams}){
+	const id = params.slug ? ' . ' + params.slug : ''
+	return{
+		title: `My thoughts ${id.replaceAll('_', ' ')}`
+	}
+}
+
+export default function thoughtPage(props) {
+	const slug = props.params.slug
+	const post = getPostContent(slug)
+	console.log(post)
+	return (
+		<main>
+			<article>
+				<h1>{post.data.title}</h1>
+				<div className="content">
+					<Markdown>{post.content}</Markdown>
+					
+				</div>
+			</article>
+		</main>
+	)
 }
