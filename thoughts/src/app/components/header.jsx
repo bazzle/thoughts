2d0d42617feb6e8a70bb72c50d4dd49500c6addd
Barry Richards <barry@br-web.me>
2024-08-18 14:52:53 +0100

    Header fixes

--- thoughts/src/app/components/header.jsx
+++ thoughts/src/app/components/header.jsx
@@ -4,17 +4,11 @@ export default function Header({ isHomePage }){
 	return (
 		<header className="header">
 			<div className="container--narrow header__inner">
+				<span className="header__site-name">
 				{
-					isHomePage ? (
-						<h1 className="header__site-name">
-							My Thoughts
-						</h1>
-					) : (
-						<span className="header__site-name">
-							<Link href={'/'}>My Thoughts</Link>
-						</span>
-					)
+					isHomePage ? <h1>My Thoughts</h1> : <Link href={'/'}>My Thoughts</Link>
 				}
+				</span>
 				<hr className="header__line" />
 			</div>
 		</header>
