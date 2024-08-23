14679bf77b6ac5d576876ba4d1fc2913e4dcfecd
Barry Richards <barry@br-web.me>
2024-08-23 14:01:25 +0100

    Updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: 'export',
-	basePath: '/thoughts'
+const nextConfig = (phase) => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+
+  return {
+    output: isDev ? 'standalone' : 'export',
+    basePath: isDev ? '' : '/thoughts',
+    // Additional configurations based on the environment can go here
+  };
 };
+
 export default nextConfig;
